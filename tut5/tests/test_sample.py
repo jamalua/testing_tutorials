58fb12b170c83f26596d0789d2f026b74a01f047
Jamal Ahmed <jamal_ua@hotmail.com>
2023-06-10 11:04:17 +0100

    commit

--- tut5/tests/test_sample.py
+++ tut5/tests/test_sample.py
@@ -3,9 +3,19 @@ import os
 from tut5.myapp.sample import save_dict
 
 
-def test_save_dict(tempdir):
-    filepath = os.path.join(tempdir, "test.json")
+def test_save_dict(tmpdir, capsys):
+    
+    """ Description
+    :type tmpdir:
+    :param tmpdir:
+
+    :raises:
+
+    :rtype:
+    """
+    filepath = os.path.join(tmpdir, "test.json")
     _dict = {"a": 1, "b": 2}
 
     save_dict(_dict, filepath)
     assert json.load(open(filepath, 'r')) == _dict
+    assert capsys.readouterr().out == "saved\n"
