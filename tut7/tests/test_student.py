7253bc277140fe1c0ccda5ddecade8d3c49345b9
Jamal Ahmed <jamal_ua@hotmail.com>
2023-06-18 13:47:11 +0100

    commit

--- tut7/tests/test_student.py
+++ tut7/tests/test_student.py
@@ -1,8 +1,5 @@
 from datetime import datetime
-from tut7.myapp.student import Student
-import pytest
-
-pytest.fixture
+from tut7.myapp.student import Student, get_topper
 
 
 def make_dummy_student():
@@ -10,3 +7,15 @@ def make_dummy_student():
         return Student(name, datetime(2000, 1, 1), "coe", credits)
 
     return _make_dummy_student
+
+
+def test_get_topper(make_dummy_student):
+    students = [
+        make_dummy_student("ram", 12),
+        make_dummy_student("shyam", 19),
+        make_dummy_student("ravi", 22),
+    ]
+
+    topper = get_topper(students)
+
+    assert topper == students[2]
